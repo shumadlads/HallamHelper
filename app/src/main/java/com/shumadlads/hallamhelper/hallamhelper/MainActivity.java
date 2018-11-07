72441fc7c7899c19ee19218d2104a5bcd345ae15
j4ckoxuk <j4ckoxuk@gmail.com>
2018-11-07 13:53:39 +0000

    Added Menus

--- app/src/main/java/com/shumadlads/hallamhelper/hallamhelper/MainActivity.java
+++ app/src/main/java/com/shumadlads/hallamhelper/hallamhelper/MainActivity.java
@@ -1,7 +1,12 @@
 package com.shumadlads.hallamhelper.hallamhelper;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        return true;
     }
 }
