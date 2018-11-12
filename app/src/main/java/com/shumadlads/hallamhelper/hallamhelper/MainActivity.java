2587dc65d85810119ca1e5683b624e9b6bb8d546
j4ckoxuk <j4ckoxuk@gmail.com>
2018-11-12 18:15:10 +0000

    Added menu functionality, added log in screen, settings and help pages, added framework for fragments

--- app/src/main/java/com/shumadlads/hallamhelper/hallamhelper/MainActivity.java
+++ app/src/main/java/com/shumadlads/hallamhelper/hallamhelper/MainActivity.java
@@ -1,24 +1,95 @@
 package com.shumadlads.hallamhelper.hallamhelper;
 
+
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+
+
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(OnNavigationItemSelectedListener);
+
+        toolbar.setTitle("Navigate");
+
+
+
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener OnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.action_navigate:
+                    toolbar.setTitle("Navigate");
+                    return true;
+                case R.id.action_timetable:
+                    toolbar.setTitle("Timetables");
+                    return true;
+                case R.id.action_slack:
+                    toolbar.setTitle("Don't Slack!");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.settings_menu, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.settings:
+                intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.help:
+                intent = new Intent(this, HelpActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.logout:
+                intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
