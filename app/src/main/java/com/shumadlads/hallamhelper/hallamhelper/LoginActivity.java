0a8d56cdd820953975622ac6cfcf04cefd845054
j4ckoxuk <j4ckoxuk@gmail.com>
2019-02-28 11:53:35 +0000

    Added error messages for incorrect login

--- app/src/main/java/com/shumadlads/hallamhelper/hallamhelper/LoginActivity.java
+++ app/src/main/java/com/shumadlads/hallamhelper/hallamhelper/LoginActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,6 +19,10 @@ public class LoginActivity extends AppCompatActivity {
         final EditText username = (EditText) findViewById(R.id.input_email);
         final EditText password = (EditText) findViewById(R.id.input_password);
         final Button button = (Button) findViewById(R.id.btn_signup);
+        final TextView txt = (TextView)findViewById(R.id.error);
+        txt.setVisibility(View.GONE);
+
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
@@ -24,7 +30,13 @@ public class LoginActivity extends AppCompatActivity {
                     startActivity(intent);
                     finish();
                     //correct password
+                }else{
+
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+                    txt.setVisibility(View.VISIBLE);
                 }
+
+
                 //wrong password
 
 
