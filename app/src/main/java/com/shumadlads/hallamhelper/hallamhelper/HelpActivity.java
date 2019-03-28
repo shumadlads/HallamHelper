9eecda9e3a7d7422bc4afc57fed640811a503719
j4ckoxuk <j4ckoxuk@gmail.com>
2019-03-28 19:35:29 +0000

    help screen finished

--- app/src/main/java/com/shumadlads/hallamhelper/hallamhelper/HelpActivity.java
+++ app/src/main/java/com/shumadlads/hallamhelper/hallamhelper/HelpActivity.java
@@ -2,6 +2,8 @@ package com.shumadlads.hallamhelper.hallamhelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help_activity);
+        Toolbar toolbar = findViewById(R.id.help_Toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Help");
     }
 }
